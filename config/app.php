1eb37647fe82b06696e675dc12903bc2d60db64f
cosmus123 <cosminmus@gmail.com>
2019-06-07 18:34:40 +0300

    app update

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('SomeRandomStringSomeRandomString'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
